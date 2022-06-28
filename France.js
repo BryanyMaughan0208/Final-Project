166d543a97c48138c3e8a694432bb28097adecdd
BryanyMaughan0208 <bryanymaughan@hotmail.com>
2022-06-28 20:11:59 +0100

    Initial commit

---
+++ France.js
@@ -0,0 +1,10 @@
+let France = prompt("Are you looking for our France travel page?");
+if(France === "yes"){
+    alert("You're in the right place! We hope you find the information you need :)");
+}
+else if(France === "no"){
+    alert("Try using our navbar to navigate to our alternate destination pages :)");
+}
+else{
+    alert("Sorry I didn't catch that, try answering yes or no :)");
+}
---
+++ Spain.js
@@ -0,0 +1,10 @@
+let Spain = prompt("Are you looking for our Spain travel page?");
+if(Spain === "yes"){
+    alert("You're in the right place! We hope you find the information you need :)");
+}
+else if(Spain === "no"){
+    alert("Try using our navbar to navigate to our alternate destination pages :)");
+}
+else{
+    alert("Sorry I didn't catch that, try answering yes or no :)");
+}
